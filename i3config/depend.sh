5b24a1dc73248dda8197ea3e86b661f190de8d8b
aerian <wo199710@hotmail.com>
2019-02-20 16:36:47 +0800

    添加依赖安装脚本

---
+++ i3config/depend.sh
@@ -0,0 +1,10 @@
+sudo apt-get install rofi
+sudo apt-get install numlockx
+sudo apt-get install xfce4-power-manager
+sudo apt-get ubstall caffeine
+sudo apt-get install volumeicon-alsa
+sudo apt-get install xautolock
+sudo apt-get install i3lock-fancy
+sudo apt-get install compton
+sudo apt-get install xfce4-notifyd
+sudo apt-get install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libiw-dev libnl-genl-3-dev
