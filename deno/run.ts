c900d4293a4fa8625f563a73b251ce17e8d7a5b6
bigshans <26884666+bigshans@users.noreply.github.com>
2022-10-10 15:54:11 +0800

    feat: add run command

---
+++ deno/run.ts
@@ -0,0 +1,27 @@
+const script = Deno.args[0];
+const ft = script.split(".").at(-1);
+
+if (typeof ft === "undefined") {
+  Deno.run({ cmd: script.split(" ") });
+  Deno.exit(0);
+}
+
+const commandDict: Record<string, string> = {
+  ts: "ts-node %f",
+  js: "node %f",
+  sh: "bash %f",
+  rs: "rustc %f -o temp && ./temp; rm temp",
+  go: "go run %f",
+  rkt: "racket %f",
+  py: "python %f",
+};
+
+if (!commandDict[ft]) {
+  console.error("Error: Unknown File Type!");
+  Deno.exit(1);
+}
+
+const command = commandDict[ft].replace(/\%f/g, script);
+console.log(`%c$ ${command}`, "font-weight: bold; color: green;");
+const p = Deno.run({ cmd: ["bash", "-c", command], cwd: "./" });
+await p.status();
