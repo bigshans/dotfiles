01f64fa0d95b5b05c94490a24576daf635533d30
bigshans <26884666+bigshans@users.noreply.github.com>
2023-07-18 18:28:48 +0800

    style: adjust the position of code

--- process/env/main.zsh
+++ process/env/main.zsh
@@ -2,22 +2,24 @@ ENV_PROJECT=$PROJECT/process/env
 export GOPROXY=https://goproxy.cn
 export GO111MODULE=on
 
-export NODE_ENV='development'
 export BROWSER="/usr/bin/firefox-nightly"
 export GOPATH=$HOME/go
 export ANDROID_SDK_ROOT=/mnt/disk/Android
 export ANDROID_HOME=/mnt/disk/Android/
 
 export PATH=$HOME/.config/emacs/bin:$PATH
-export PATH=$PATH:~/.config/yarn/global/node_modules/.bin
 export SONAR_SCANNER_HOME="/opt/sonar-scanner"
 export METALS_JAVA_OPTS='-XX:+UseG1GC -XX:+UseStringDeduplication -Xss4m -Xms100m'
 
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
+export PATH=$PATH:~/.config/yarn/global/node_modules/.bin
+export NODE_ENV='development'
 export N_PREFIX=$HOME/.n
 export NPM_PACKAGES="$HOME/.npm-packages"
 
+# ruby lib
 export PATH=$GOPATH/bin:$PATH
 IN=$(gem env gempath)
 ps=(`echo $IN | tr ':' ' '`)
@@ -26,6 +28,7 @@ do
     p=$p/bin
     export PATH=$p:$PATH
 done
+
 export PATH=$PATH:$HOME/dotfiles/scripts
 export PATH=$PATH:$HOME/.local/bin
 export PATH="${PATH}:${SONAR_SCANNER_HOME}/bin"
