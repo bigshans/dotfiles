f62519d63817d4ed045d72117854ebf742dcf486
bigshans <26884666+bigshans@users.noreply.github.com>
2024-03-14 23:52:22 +0800

    style: format code

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,62 +7,62 @@ local function K(prefix, key, action)
     return { key = key, mods = prefix, action = action }
 end
 
-local K_CTSH = function (key, action)
+local K_CTSH = function(key, action)
     return K('CTRL|SHIFT', key, action)
 end
 
-local K_C = function (key, action)
+local K_C = function(key, action)
     return K('CTRL', key, action)
 end
 
-local K_CTAL = function (key, action)
-  return K('CTRL|ALT', key, action)
+local K_CTAL = function(key, action)
+    return K('CTRL|ALT', key, action)
 end
 
 local keys = {
-    K_CTSH('(', act.SplitHorizontal { domain = 'CurrentPaneDomain' }),
-    K_CTSH(')', act.SplitVertical { domain = 'CurrentPaneDomain' }),
-    K_CTSH('x', act.TogglePaneZoomState ),
-    K('CTRL', 'w', act.CloseCurrentPane { confirm = true }),
-    K('CTRL', 'q', act.CloseCurrentTab { confirm = true }),
+    K_CTSH('(', act.SplitHorizontal({ domain = 'CurrentPaneDomain' })),
+    K_CTSH(')', act.SplitVertical({ domain = 'CurrentPaneDomain' })),
+    K_CTSH('x', act.TogglePaneZoomState),
+    K('CTRL', 'w', act.CloseCurrentPane({ confirm = true })),
+    K('CTRL', 'q', act.CloseCurrentTab({ confirm = true })),
     K_CTSH('z', act.ActivateCopyMode),
-   -- activate pane selection mode with numeric labels
-   K_C('9', act.PaneSelect),
+    -- activate pane selection mode with numeric labels
+    K_C('9', act.PaneSelect),
     -- show the pane selection mode, but have it swap the active and selected panes
-    K_C('0', act.PaneSelect { mode = 'SwapWithActive' })
+    K_C('0', act.PaneSelect({ mode = 'SwapWithActive' })),
 }
 
-for _, v in ipairs {'Left', 'Right', 'Up', 'Down'} do
+for _, v in ipairs({ 'Left', 'Right', 'Up', 'Down' }) do
     table.insert(keys, K('ALT', v .. 'Arrow', act.ActivatePaneDirection(v)))
 end
 
 for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(keys, {
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = act.ActivateTab(i - 1),
-  })
+    -- CTRL+ALT + number to activate that tab
+    table.insert(keys, {
+        key = tostring(i),
+        mods = 'CTRL|ALT',
+        action = act.ActivateTab(i - 1),
+    })
 end
 
 return {
     -- 修复 vim 下使用 C-d 后 Meta 键失灵的问题。1
     enable_csi_u_key_encoding = true,
     keys = keys,
-  mouse_bindings = {
-    -- Bind 'Up' event of CTRL-Click to open hyperlinks
-    {
-      event = { Up = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
-      action = act.OpenLinkAtMouseCursor,
+    mouse_bindings = {
+        -- Bind 'Up' event of CTRL-Click to open hyperlinks
+        {
+            event = { Up = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = act.OpenLinkAtMouseCursor,
+        },
+        -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+        {
+            event = { Down = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = act.Nop,
+        },
     },
-    -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
-    {
-      event = { Down = { streak = 1, button = 'Left' } },
-      mods = 'CTRL',
-      action = act.Nop,
-    },
-  },
     -- 隐藏标题
     -- window_decorations = "NONE",
     colors = darcula,
@@ -73,12 +73,12 @@ return {
     window_background_opacity = 0.85,
     font = wezterm.font_with_fallback({
         -- 测试字符串 oO01Ili
-        { family = 'monospace' },
+        { family = 'Cascadia Code' },
         'Noto Sans CJK SC',
         'Source Code Pro',
         'Iosevka Nerd Font',
         'Hack Nerd Font',
-        'FuraMono Nerd Font'
+        'FuraMono Nerd Font',
     }),
     harfbuzz_features = { 'zero' },
     font_size = 13.5,
